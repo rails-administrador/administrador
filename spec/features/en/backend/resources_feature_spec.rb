a23035a7f33738aefb74e6a77eabd91f9048efd9
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-01-11 17:14:38 +0100

    #wip

--- spec/features/en/backend/resources_feature_spec.rb
+++ spec/features/en/backend/resources_feature_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe '/en/backend', type: :feature do
-  let(:base_path) { '/en/backend' }
+  include ActionView::RecordIdentifier
 
-  before(:each) { visit(base_path) }
+  let(:administrador_root_path) { '/en/backend' }
+  let(:engine) { Administrador::Configuration.engines['News::Backend::Engine'] }
+  let(:resource_class) { News::Item }
+  let(:base_path) { '/news-backend/items' }
 
-  it { expect(page.status_code).to eq(200) }
-  it { expect(current_path).to eq(base_path) }
+  describe 'index' do
+    before(:each) do
+      visit(administrador_root_path)
+      within("##{dom_id(engine)}") { click_link(resource_class.model_name.human(count: :other)) }
+    end
+
+    it { expect(page.status_code).to eq(200) }
+    it { expect(current_path).to eq(base_path) }
+  end
 end
