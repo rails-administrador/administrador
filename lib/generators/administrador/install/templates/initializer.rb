09db4d70645a230ce588cbf5324b8f1ab1ff5032
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-03-31 22:50:13 +0200

    Change default resource_label_methods defaults to make it more versatile.

--- lib/generators/administrador/install/templates/initializer.rb
+++ lib/generators/administrador/install/templates/initializer.rb
@@ -1,9 +1,9 @@
 Administrador.configure do |config|
   # Methods, that will be tried on a resource to generate a nice title
   #
-  # Default: config.resource_label_methods = [:human, :name, :email, :to_s]
+  # Default: config.resource_label_methods = [:administrador_title, :human, :name, :identifier, :to_s]
   #
-  config.resource_label_methods = [:human, :name, :email, :to_s]
+  config.resource_label_methods = [:administrador_title, :human, :name, :identifier, :to_s]
 
   # config.register_engine 'Ecm::Blog::Backend::Engine', {}
 
