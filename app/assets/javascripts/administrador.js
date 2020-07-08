7599ecbbe11213e4b70e4f76960067c421c48216
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-08 22:20:14 +0200

    Add simple_form-datetimepicker as dependency. Add date and date time picker via simple_form-datetimepicker.

--- app/assets/javascripts/administrador.js
+++ app/assets/javascripts/administrador.js
@@ -3,4 +3,5 @@
 //= require rao-component/acts_as_list
 //= require rao-component/awesome_nested_set
 //= require simple_sidebar
+//= require simple_form-datetimepicker
 //= require administrador/application
